22488203bef8febd4d70be2eb0154bbae6d5a6eb
Chase Moskal <chasemoskal@gmail.com>
2025-09-27 18:00:28 -0700

    fix: exports

--- s/index.ts
+++ s/index.ts
@@ -1,6 +1,7 @@
 
 export * from "./themes/index.js"
 export * from "./ui/example/component.js"
+export * from "./ui/copy/component.js"
 export * from "./ui/themers.js"
 export * from "./ui/components.js"
 
