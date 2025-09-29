3e73e572c230a27903ef32149ba02a089ac839f1
Chase Moskal <chasemoskal@gmail.com>
2025-09-28 23:13:45 -0700

    fix: component exports

--- s/index.ts
+++ s/index.ts
@@ -1,9 +1,12 @@
 
 export * from "./themes/index.js"
 
-export * from "./components/example/component.js"
 export * from "./components/copy/component.js"
+export * from "./components/drawer/component.js"
+export * from "./components/drawer/drawer.js"
+export * from "./components/example/component.js"
 export * from "./components/framework.js"
+export * from "./components/raw-components.js"
 
 export * from "./shiny.js"
 
