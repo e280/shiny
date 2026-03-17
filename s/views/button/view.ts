8a05cc8eeef8b74346ad6f5d0e406c3d54fdfa7c
Chase Moskal <chasemoskal@gmail.com>
2026-03-16 20:54:04 -0700

    add: shiny button onClick

--- s/views/button/view.ts
+++ s/views/button/view.ts
@@ -7,6 +7,7 @@ import {themeCss} from "../../theme/theme.css.js"
 
 export const ShinyButton = shadow((content?: Content, options: {
 		vibe?: "calm" | "lame" | "happy" | "sad" | "angry" | "zesty" | "quirky"
+		onClick?: (event: PointerEvent) => void
 	} = {}) => {
 
 	useName("shiny-button")
@@ -22,7 +23,8 @@ export const ShinyButton = shadow((content?: Content, options: {
 			part=button
 			data-vibe="${options.vibe ?? "calm"}"
 			?disabled="${attrs.disabled}"
-			?hidden="${attrs.hidden}">
+			?hidden="${attrs.hidden}"
+			@click="${options.onClick}">
 			<slot>${content}</slot>
 		</button>
 	`
