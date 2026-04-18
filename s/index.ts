a25abac27bff74d454ebaccfc4ebc246bb86cfc8
Chase Moskal <chasemoskal@gmail.com>
2026-04-17 20:25:28 -0700

    export: tab and drawer controls

--- s/index.ts
+++ s/index.ts
@@ -1,6 +1,8 @@
 
 export * from "./views/button/view.js"
 export * from "./views/copy/view.js"
+export * from "./views/drawer/control.js"
 export * from "./views/drawer/view.js"
+export * from "./views/tabs/control.js"
 export * from "./views/tabs/view.js"
 
